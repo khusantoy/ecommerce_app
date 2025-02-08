adcb9bc0469100631e7bbd3187649ae62295c5ea
xusanboy <tursunovkhusanboy@gmail.com>
2025-02-08 17:19:03 +0500

    configure theme mode

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,24 @@
+import 'package:ecommerce_app/utils/theme/custom_theme/text_theme.dart';
+import 'package:flutter/material.dart';
+
+class TAppTheme {
+  TAppTheme._();
+
+  static ThemeData lighTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TTextTheme.lightTextTheme,
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.dark,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: TTextTheme.darkTextTheme,
+  );
+}

efb5551fa07596f0e094bc0b66d21e649305ec04
xusanboy <tursunovkhusanboy@gmail.com>
2025-02-09 17:28:03 +0500

    add color, image string, size, text string constants

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,10 @@
-import 'package:ecommerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
 
+import 'app.dart';
+
+/// ------ Entry point of Flutter App -----
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-  
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: TAppTheme.lighTheme,
-      darkTheme: TAppTheme.darkTheme,
-    );
-  }
-}
